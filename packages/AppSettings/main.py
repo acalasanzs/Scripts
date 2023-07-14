c6397f2e739e1424e315696c7ad7eef23230a933
Albert Calasanz <acalasanzs@gmail.com>
2023-07-14 16:49:13 +0200

    A

--- packages/AppSettings/main.py
+++ packages/AppSettings/main.py
@@ -1,19 +1,26 @@
 import json, os
-from packages.AppSettings.utils import staticinstance
+from typing import Any, Callable, List
+# from packages.AppSettings.utils import staticinstance
 
-class Attribute():
-    def __init__(self, attr, validate):
+class Attribute:
+    def __init__ (self, attr, typ : type | None, validate:  Callable[[object], bool] | None]):
         self.attr = attr
-        self.validate = validate
+        if typ is None and validate is Callable[[object], bool]:
+            self.validate = validate
+        elif validate is None and typ is type:
+            self.validate = lambda a: a is typ
+        else:
+            raise SystemExit("No type!")
 class Option():
     def __init__(self, name: str):
         self.name = name
-    def append(self, name, option):
-        pass
+        self.attributes = []
+    def append(self, attribute: Attribute):
+        self.attributes.append(attribute)
 class AppSettings():
-    def __init__(self, options: list):
+    def __init__(self, options: List[Option]):
         pass
-    @staticinstance
-    def load(self, filename = "settings.json", path = os.getcwd()):
+    @staticmethod
+    def load(filename = "settings.json", path = os.getcwd()):
         return json.load(open(os.path.join(path,filename)))
         
